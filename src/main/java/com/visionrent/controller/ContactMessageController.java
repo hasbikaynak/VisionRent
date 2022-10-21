8dab880d087e0ac73dc6bcb9a71fca0d81d56db5
hasbikaynak <kaynakhasbi1@gmail.com>
2022-10-21 11:22:05 +0200

    create: ApiResponseError.java, VisionRentExceptionHandler.java and ErrorMessage.java

--- src/main/java/com/visionrent/controller/ContactMessageController.java
+++ src/main/java/com/visionrent/controller/ContactMessageController.java
@@ -1,19 +1,36 @@
 package com.visionrent.controller;
 
+import java.util.List;
+import java.util.function.Function;
+
+import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.visionrent.domain.ContactMessage;
+import com.visionrent.dto.ContactMessageDTO;
 import com.visionrent.dto.request.ContactMessageRequest;
+import com.visionrent.dto.response.ResponseMessage;
 import com.visionrent.dto.response.VRResponse;
 import com.visionrent.mapper.ContactMessageMapper;
 import com.visionrent.service.ContactMessageService;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/contactmessage")
@@ -21,17 +38,96 @@ import javax.validation.Valid;
 public class ContactMessageController {
 
     private ContactMessageService contactMessageService;
+
     private ContactMessageMapper contactMessageMapper;
 
 
-    // POST  localhost:8080/contactmessage/visitors
     @PostMapping("/visitors")
     public ResponseEntity<VRResponse> createMessage(@Valid @RequestBody ContactMessageRequest contactMessageRequest){
+
         ContactMessage contactMessage = contactMessageMapper.contactMessageRequestToContactMessage(contactMessageRequest);
         contactMessageService.saveMessage(contactMessage);
 
-        VRResponse response = new VRResponse("ContactMessage successfully created",true);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        VRResponse response=new VRResponse(ResponseMessage.CONTACTMESSAGE_SAVE_RESPONSE_MESSAGE, true);
+
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<ContactMessageDTO>> getAllContactMessage(){
+        List<ContactMessage> contactMessageList= contactMessageService.getAll();
+        List<ContactMessageDTO> contactMessageDTOList = contactMessageMapper.map(contactMessageList);
+
+        return ResponseEntity.ok(contactMessageDTOList);
+
+    }
+
+    //http://localhost:8080/contactmessage/pages?page=0&size=1&sort=id&direction=ASC
+
+    @GetMapping("/pages")
+    public ResponseEntity<Page<ContactMessageDTO>> getAllContactMessageWithPage(@RequestParam("page") int page,@RequestParam("size") int size,
+                                                                                @RequestParam("sort") String prop,
+                                                                                @RequestParam(value="direction",required=false,defaultValue = "DESC") Direction direction){
+        Pageable pageable=PageRequest.of(page, size,Sort.by(direction,prop));
+        Page<ContactMessage> contactMessagePage = contactMessageService.getAll(pageable);
+
+        //Page<ContactMessage> -> Page<ContactMessageDTO>
+        Page<ContactMessageDTO> pageDTO = getPageDTO(contactMessagePage);
+
+        return ResponseEntity.ok(pageDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactMessageDTO> getMessageWithPath(@PathVariable("id") Long id){
+        ContactMessage contactMessage = contactMessageService.getContactMessage(id);
+        ContactMessageDTO contactMessageDTO = contactMessageMapper.contactMessageToDTO(contactMessage);
+
+        return ResponseEntity.ok(contactMessageDTO);
+    }
+
+    @GetMapping("/request")
+    public ResponseEntity<ContactMessageDTO> getMessageWithRequestParam(@RequestParam("id") Long id){
+        ContactMessage contactMessage = contactMessageService.getContactMessage(id);
+        ContactMessageDTO contactMessageDTO = contactMessageMapper.contactMessageToDTO(contactMessage);
+
+        return ResponseEntity.ok(contactMessageDTO);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<VRResponse> deleteContactMessage(@PathVariable Long id){
+        contactMessageService.deleteContactMessage(id);
+
+        VRResponse vrResponse=new VRResponse(ResponseMessage.CONTACTMESSAGE_DELETE_RESPONSE_MESSAGE, true);
+        return ResponseEntity.ok(vrResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<VRResponse> updateContactMessage(@PathVariable Long id,@Valid @RequestBody ContactMessageRequest contactMessageRequest ){
+
+        ContactMessage contactMessage= contactMessageMapper.contactMessageRequestToContactMessage(contactMessageRequest);
+
+        contactMessageService.updateContactMessage(id, contactMessage);
+
+        VRResponse vrResponse=new VRResponse(ResponseMessage.CONTACTMESSAGE_UPDATE_RESPONSE_MESSAGE, true);
+        return ResponseEntity.ok(vrResponse);
+
+    }
+
+
+
+    private Page<ContactMessageDTO> getPageDTO(Page<ContactMessage>  contactMessagePage){
+        Page<ContactMessageDTO> dtoPage= contactMessagePage.map( new Function<ContactMessage, ContactMessageDTO>() {
+            @Override
+            public ContactMessageDTO apply(ContactMessage contactMessage) {
+                return contactMessageMapper.contactMessageToDTO(contactMessage);
+            }
+        });
+
+        return dtoPage;
+    }
+
+
 
 }
--- src/main/java/com/visionrent/dto/request/ContactMessageRequest.java
+++ src/main/java/com/visionrent/dto/request/ContactMessageRequest.java
@@ -5,22 +5,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContactMessageRequest {
+    @Size(min=1,max = 50, message = "Your Name '${validatedValue}' must be between {min} and {max} chars long")
     @NotBlank(message = "Please provide a name")
     private String name;
 
+    @Size(min=1,max = 50, message = "Subject '${validatedValue}' must be between {min} and {max} chars long")
     @NotBlank(message = "Please provide a subject")
     private String subject;
 
+    @Size(min=1,max = 50, message = "Message Body '${validatedValue}' must be between {min} and {max} chars long")
     @NotBlank(message = "Please provide a body")
     private String body;
 
+    @Email(message = "Please provide a Email")
     @NotBlank(message = "Please provide a email")
     private String email;
 }
